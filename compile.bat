1b688ae7ba630b50e06337cf371ed703c87be348
im1sha <vkporsche@ya.ru>
2018-11-12 01:30:15 +0300

    script to 10th task added

---
+++ compile.bat
@@ -0,0 +1,8 @@
+
+mkdir task9compiled
+
+mkdir task9compiled\bin
+
+javac -sourcepath .\src -d .\task9compiled\bin .\src\by\bsuir\ovchelupov\task09\Runner.java
+
+java -classpath .\task9compiled\bin by.bsuir.ovchelupov.task09.Runner
